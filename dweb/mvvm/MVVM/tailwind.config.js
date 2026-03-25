486531399580a6150bf10fd5449197cf0619b6c6
Afonso Costa <afonso2004costa@gmail.com>
2026-03-25 10:24:09 +0000

    recreation of MVVM project

---
+++ dweb/mvvm/MVVM/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./Pages/**/*.cshtml",
+        "./Views/**/*.cshtml",
+        "./Areas/**/*.cshtml",
+        "./Pages/**/*.razor",
+        
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
